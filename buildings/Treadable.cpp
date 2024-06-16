1c5ab20e081eef0a6cdc9c319b84aeaaafc8127c
Erik <81354428+X64X2@users.noreply.github.com>
2024-06-16 08:45:56 +0000

    Delete buildings directory

--- buildings/Treadable.cpp
+++
@@ -1,8 +0,0 @@
-#include "common.h"
-
-#include "rpworld.h"
-#include "Treadable.h"
-#include "Pools.h"
-
-void *CTreadable::operator new(size_t sz) throw() { return CPools::GetTreadablePool()->New();  }
-void CTreadable::operator delete(void *p, size_t sz) throw() { CPools::GetTreadablePool()->Delete((CTreadable*)p); }
